f50da21dd6fbc0ce28e1439323a631933e9fe2d8
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-02-07 17:31:40 +0000

    style: add navbar styles :art:

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,14 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "./header.css"
 
 const Header = ({ siteTitle }) => (
   <header className="Header">
     <div className="HeaderGroup">
       <Link to="/">
         <img src={(require('../images/logo-designcode.svg'))} width="30" alt=""/>
-        </Link>
+      </Link>
       <Link to="/courses">Courses</Link>
       <Link to="/downloads">Downloads</Link>
       <Link to="/workshops">Workshops</Link>
