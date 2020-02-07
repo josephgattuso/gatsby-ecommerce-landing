ec1a5e62a1d359fbcb9b2949c1cc92460fe81b13
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-02-07 16:37:36 +0000

    feat: add hero section and styling :art:

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,20 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Learn to <br />design and code<br/>React apps</h1>
-    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
-    <Link to="/about/">Watch the video &rarr;</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>
+          Learn to design and code<br /><span>modern apps</span>
+        </h1>
+        <p>
+          Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.
+        </p>
+        <Link to="/about/">
+          Watch the video &rarr;
+        </Link>
+      </div>
+    </div>
+
   </Layout>
 )
 
